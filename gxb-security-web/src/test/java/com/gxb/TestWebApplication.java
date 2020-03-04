8cf7e5e4fb12700b8422b9b269bd4b5e5a2cc8db
Geligamesh <535758155@qq.com>
2020-03-04 23:35:07 +0800

    实现短信验证码登录，账号密码登录，限制多人登录，强行下线等功能

---
+++ gxb-security-web/src/test/java/com/gxb/TestWebApplication.java
@@ -0,0 +1,41 @@
+package com.gxb;
+
+import com.gxb.web.entities.SysPermission;
+import com.gxb.web.entities.SysUser;
+import com.gxb.web.service.SysPermissionService;
+import com.gxb.web.service.SysUserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TestWebApplication {
+
+    @Autowired
+    private SysUserService sysUserService;
+    @Autowired
+    private SysPermissionService sysPermissionService;
+
+    @Test
+    public void testSysUser() {
+        List<SysUser> list = sysUserService.list();
+        System.out.println("list:" + list);
+    }
+
+    @Test
+    public void testFindByUsername() {
+        SysUser admin = sysUserService.findByUsername("admin");
+        System.out.println(admin);
+    }
+
+    @Test
+    public void testSysPer() {
+        List<SysPermission> permissions = sysPermissionService.findByUserId(99l);
+        System.out.println("size:" + permissions.size());
+    }
+}
