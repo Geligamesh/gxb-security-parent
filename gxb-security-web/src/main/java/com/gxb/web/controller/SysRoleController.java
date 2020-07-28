6beb09de72f87f39c6a85ff7bceb38125bade97d
Geligamesh <535758155@qq.com>
2020-07-29 00:07:51 +0800

    权限操作

--- gxb-security-web/src/main/java/com/gxb/web/controller/SysRoleController.java
+++ gxb-security-web/src/main/java/com/gxb/web/controller/SysRoleController.java
@@ -1,5 +1,6 @@
 package com.gxb.web.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("role")
 @Controller
+@Slf4j
 public class SysRoleController {
 
     private static final String HTML_PREFIX = "system/role/";
